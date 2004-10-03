8ed3f0c24e98140341e71942a239eabc5abf9458
Roger Koenker <rkoenker@uiuc.edu>
2004-10-03 00:00:00 +0000

    version 0.63

---
+++ R/defunct.R
@@ -0,0 +1,5 @@
+#-------------------------------------------------------------------------------
+".First.lib" <-
+function(lib, pkg) {
+   cat("nprq is a defunct package:  functionality now folded into the package quantreg")
+}
